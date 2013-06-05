b8a6ee47dfeaeabb847f521d946806124f66caff
Brian Trammell <brian@trammell.ch>
2013-06-05 08:48:59 +0200

    test harness tweaks

--- reader_test.py
+++ reader_test.py
@@ -2,11 +2,15 @@
 
 import ipfix.reader
 import ipfix.ie
+import pprint
 
 ipfix.ie.use_iana_default()
 ipfix.ie.use_5103_default()
+ipfix.ie.load_specfile("../qof/test/qof.iespec")
 
 r = ipfix.reader.from_stream(open("../qof/yaf-headers.ipfix", mode="rb"))
 
+pp = pprint.PrettyPrinter(indent = 2)
+
 for rec in r.dict_iterator():
-    print(rec)
+    pp.pprint(rec)
