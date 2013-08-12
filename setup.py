3d3ff1cea25d627b4c25f93a937cec88deadbbf1
Brian Trammell <trammell@tik.ee.ethz.ch>
2013-08-12 12:36:51 +0200

    prep for 0.9.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.txt') as file:
     long_description = file.read()
 
 setup(name='ipfix',
-      version='0.9.3',
+      version='0.9.4',
       description='IPFIX implementation for Python 3.3+',
       long_description = long_description,
       author='Brian Trammell',
@@ -14,7 +14,7 @@ setup(name='ipfix',
       url='http://github.com/britram/python-ipfix',
       packages=['ipfix'],
       package_data={'ipfix': ['iana.iespec', 'rfc5103.iespec']},
-      scripts=['scripts/ipfix2csv'],
+      scripts=['scripts/ipfix2csv', 'scripts/nf92csv'],
       classifiers=["Development Status :: 3 - Alpha",
                    "Intended Audience :: Developers",
                    "License :: OSI Approved :: "
